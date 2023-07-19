72bf638df3c0ea90b26e6b5cf4fed00a674a9b6e
joxenz <egorovkirill846@gmail.com>
2023-07-19 20:07:05 +0300

    asg

---
+++ Main.java
@@ -0,0 +1,22 @@
+    import java.util.ArrayList;
+
+class Programm {
+    public static void main(String[] args) {
+            ArrayList<String> statesCapitals = new ArrayList<>();
+            statesCapitals.add("Канада");
+            statesCapitals.add("Ирак");
+            statesCapitals.add("Австрия");
+            statesCapitals.add("К1");
+            statesCapitals.add("Ка2нада");
+            statesCapitals.add("Кана3да");
+            System.out.println(statesCapitals.contains("Канада"));
+
+
+        }
+    public static void readPage(Integer page){
+        page++;
+        System.out.println(page);
+        int x = 0;
+    }
+
+}
